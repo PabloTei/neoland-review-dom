ba078d74ed712ee041adc7415e212c99c8b539dd
PabloTei <pteijeirolopez@gmail.com>
2023-01-26 16:46:24 +0100

    iteration10

--- main.js
+++ main.js
@@ -33,7 +33,6 @@ for (const car of cars) { //Creamos un bucle para recorrer el array
 }
 
 
-
 // 1.4 Crea dinamicamente en el html una lista de div que contenga un elemento h4 para el titulo y otro elemento img para la imagen.
 
 const countries1 = [
@@ -44,6 +43,7 @@ const countries1 = [
 	{title: 'Random title', imgUrl: 'https://picsum.photos/300/200?random=5'}
 ];
 
+let = 0;
 for (const country of countries1) {
     const createDiv = document.createElement("div"); //Creamos por cada elemento del array, guardado en memoria
     document.body.appendChild(createDiv); //Añadimos el div en el body del DOM
@@ -54,22 +54,31 @@ for (const country of countries1) {
     const img1 = document.createElement("img"); //Creamos por cada elemento del array una imagen, guardado en memoria
     createList.appendChild(img1); //Añadimos dentro la lista la imagen de cada elemento del array
     tituloH4.insertAdjacentHTML("afterbegin", country.title);//Imprimimos el titulo
-    img1.setAttribute("src", country.imgUrl);
+    img1.setAttribute("src", country.imgUrl); //Añadimos atributo src para que imprima las imágenes
     img1.insertAdjacentHTML("afterbegin", country.imgUrl);//Imprimimos la imagen
 }
 
 
-
 // 1.5 Basandote en el ejercicio anterior. Crea un botón que elimine el último elemento de la lista.
 
-
-
-
-
-
-
-
+const boton = document.createElement("button");//Creamos botón
+document.body.appendChild(boton);//Añadimos botón al DOM
+const divs = document.querySelectorAll("div");//Almacenamos todos lod DIVS
+for (let i = 0; i < divs.length; i++) {//Creamos bucle para que cuando llegue a la ultima posición a ese div le asigne el id "ultimo"
+    if(i === divs.length - 1) {
+        const ultimoDiv = divs[i].setAttribute("id", "ultimo");
+        
+    }
+}
+boton.addEventListener("click", (event) => {
+     //Creamos el evento click
+    const ultimo = document.querySelector("#ultimo");//Metemos en una variable el div con el id "ultimo"
+    ultimo.remove();// eliminamos el div con la variable ultimo
+});
 
 
 
 // 1.6 Basandote en el ejercicio anterior. Crea un botón para cada uno de los elementos de las listas que elimine ese mismo elemento del html.
+
+const newBoton = document.createElement("button");
+document.body.appendChild(newBoton);
